d412840c1365b5abb48f12203bb77a4aa9cb56cb
fan9704 <cxz123499@gmail.com>
2022-02-27 10:35:43 +0800

    Add NAV components

--- src/pages/AllMeetups.tsx
+++ src/pages/AllMeetups.tsx
@@ -1,5 +1,49 @@
+import { useState, useEffect } from "react";
+
+import MeetupList from "../components/meetups/MeetupList";
+
 function AllMeetupsPage() {
-    return <div>All Meetups Page</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-meetup-example-7f0c9-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
   }
-  
-  export default AllMeetupsPage;
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
--- src/pages/NewMeetup.tsx
+++ src/pages/NewMeetup.tsx
@@ -1,5 +1,33 @@
+// React Router
+import { useHistory } from "react-router-dom";
+
+// Components
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>;
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-example-7f0c9-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
   }
-  
-  export default NewMeetupPage;
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
